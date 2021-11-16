1f8bc1ca9b194db48e591059bd76f127b60f3e30
SaadHassan117 <saadh9290@gmail.com>
2021-11-17 00:39:23 +0500

    get string

--- app/src/main/java/com/example/tosteas/MainActivity.java
+++ app/src/main/java/com/example/tosteas/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.tosteas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[]labels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        labels= getResources().getStringArray(R.array.labels);
+        //labels= res.getStringArray(R.array.labels);
         Toast toast = Toast.makeText(this, "Toast Text",
                 Toast.LENGTH_LONG);
 
