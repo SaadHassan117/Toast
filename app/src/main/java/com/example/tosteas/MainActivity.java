3eb3e53f7768884f84c2f5e54435949b97d210a7
SaadHassan117 <saadh9290@gmail.com>
2021-11-17 19:18:16 +0500

    adaptor

--- app/src/main/java/com/example/tosteas/MainActivity.java
+++ app/src/main/java/com/example/tosteas/MainActivity.java
@@ -7,54 +7,28 @@ import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    String[]labels;
-    Button btn;
+   ArrayList< String> aray;
+   ListView obj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        labels= getResources().getStringArray(R.array.labels);
-        //labels= res.getStringArray(R.array.labels);
-        Toast toast = Toast.makeText(this, "Toast Text",
-                Toast.LENGTH_LONG);
-
-        toast.show();
-        btn=findViewById(R.id.button);
-
-        // Create the object of AlertDialog Builder class
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        obj=findViewById(R.id.list);
+              aray=new ArrayList<>();
+              aray.add ("saad");
+              aray.add("hassan");
+        ArrayAdapter<String> adaptor=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,aray);
+            obj.setAdapter(adaptor);
 
 
-                builder.setMessage("Do you want to continue");
-
-                builder.setTitle("Alert !");
-
-
-                builder.setCancelable(false);
-                builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                   finish();
-                    }
-                });
-                builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                AlertDialog alertDialog= builder.create();
-                alertDialog.show();
-
-            }
-        });
 
 
     }
